45a60dd6392f1356ce803f348ff99e8a5bb48597
Edvard Chen <pigeon73101@gmail.com>
2021-08-23 00:21:35 +0800

    fix: drop wrong ts supports
    closes #52, closes #47

--- tests/lib/rules/no-literal-string.js
+++ tests/lib/rules/no-literal-string.js
@@ -27,7 +27,7 @@ var ruleTester = new RuleTester({
     },
   },
 });
-ruleTester.run('no-literal-string', rule, {
+const usual = {
   valid: [
     { code: 'import("hello")' },
     { code: 'import(`hello`)', options: [{ validateTemplate: true }] },
@@ -84,48 +84,9 @@ ruleTester.run('no-literal-string', rule, {
     { code: 'var a = {foo: "FOO"};' },
     { code: 'var a = {foo: "foo"};', options: [{ ignoreProperty: ['foo'] }] },
     { code: 'class Form extends Component { displayName = "FormContainer" };' },
-    // JSX
-    { code: '<div className="primary"></div>' },
-    { code: '<div className={a ? "active": "inactive"}></div>' },
-    { code: '<div>{i18next.t("foo")}</div>' },
-    { code: '<svg viewBox="0 0 20 40"></svg>' },
-    { code: '<line x1="0" y1="0" x2="10" y2="20" />' },
-    { code: '<path d="M10 10" />' },
-    {
-      code:
-        '<circle width="16px" height="16px" cx="10" cy="10" r="2" fill="red" />',
-    },
-    {
-      code:
-        '<a href="https://google.com" target="_blank" rel="noreferrer noopener"></a>',
-    },
-    {
-      code: '<div id="some-id" tabIndex="0" aria-labelledby="label-id"></div>',
-    },
-    { code: '<div role="button"></div>' },
-    { code: '<img src="./image.png" />' },
-    { code: '<A style="bar" />' },
-    { code: '<button type="button" for="form-id" />' },
-    { code: '<DIV foo="bar" />', options: [{ ignoreAttribute: ['foo'] }] },
-    { code: '<Trans>foo</Trans>' },
-    { code: '<Trans><span>bar</span></Trans>' },
-    { code: '<Trans>foo</Trans>', options: [{ ignoreComponent: ['Icon'] }] },
-    { code: '<Icon>arrow</Icon>', options: [{ ignoreComponent: ['Icon'] }] },
     { code: 'a + "b"', options: [{ markupOnly: true }] },
-    { code: '<div>{import("abc")}</div>', options: [{ markupOnly: true }] },
-    {
-      code: '<div>{[].map(item=>"abc")}</div>',
-      options: [{ markupOnly: true }],
-    },
-    { code: '<div>{"hello" + "world"}</div>', options: [{ markupOnly: true }] },
-    { code: '<DIV foo="FOO" />', options: [{ markupOnly: true }] },
-    {
-      code: '<DIV foo="bar" />',
-      options: [{ markupOnly: true, ignoreAttribute: ['foo'] }],
-    },
     // when onlyAttribute was configured, the markOnly would be treated as true
     { code: 'const a = "foo";', options: [{ onlyAttribute: ['bar'] }] },
-    { code: '<DIV foo="bar" />', options: [{ onlyAttribute: ['bar'] }] },
     {
       code: 'var a = `hello world`',
     },
@@ -170,7 +131,51 @@ ruleTester.run('no-literal-string', rule, {
       code: 'class Form extends Component { property = "Something" };',
       errors,
     },
-    // JSX
+  ],
+};
+
+const jsx = {
+  valid: [
+    { code: '<div className="primary"></div>' },
+    { code: '<div className={a ? "active": "inactive"}></div>' },
+    { code: '<div>{i18next.t("foo")}</div>' },
+    { code: '<svg viewBox="0 0 20 40"></svg>' },
+    { code: '<line x1="0" y1="0" x2="10" y2="20" />' },
+    { code: '<path d="M10 10" />' },
+    {
+      code:
+        '<circle width="16px" height="16px" cx="10" cy="10" r="2" fill="red" />',
+    },
+    {
+      code:
+        '<a href="https://google.com" target="_blank" rel="noreferrer noopener"></a>',
+    },
+    {
+      code: '<div id="some-id" tabIndex="0" aria-labelledby="label-id"></div>',
+    },
+    { code: '<div role="button"></div>' },
+    { code: '<img src="./image.png" />' },
+    { code: '<A style="bar" />' },
+    { code: '<button type="button" for="form-id" />' },
+    { code: '<DIV foo="bar" />', options: [{ ignoreAttribute: ['foo'] }] },
+    { code: '<Trans>foo</Trans>' },
+    { code: '<Trans><span>bar</span></Trans>' },
+    { code: '<Trans>foo</Trans>', options: [{ ignoreComponent: ['Icon'] }] },
+    { code: '<Icon>arrow</Icon>', options: [{ ignoreComponent: ['Icon'] }] },
+    { code: '<div>{import("abc")}</div>', options: [{ markupOnly: true }] },
+    {
+      code: '<div>{[].map(item=>"abc")}</div>',
+      options: [{ markupOnly: true }],
+    },
+    { code: '<div>{"hello" + "world"}</div>', options: [{ markupOnly: true }] },
+    { code: '<DIV foo="FOO" />', options: [{ markupOnly: true }] },
+    {
+      code: '<DIV foo="bar" />',
+      options: [{ markupOnly: true, ignoreAttribute: ['foo'] }],
+    },
+    { code: '<DIV foo="bar" />', options: [{ onlyAttribute: ['bar'] }] },
+  ],
+  invalid: [
     { code: '<div>foo</div>', errors },
     { code: '<div>foo</div>', options: [{ markupOnly: true }], errors },
     { code: '<>foo999</>', options: [{ markupOnly: true }], errors },
@@ -187,7 +192,9 @@ ruleTester.run('no-literal-string', rule, {
     { code: '<img src="./image.png" alt="some-image" />', errors },
     { code: '<button aria-label="Close" type="button" />', errors },
   ],
-});
+};
+ruleTester.run('no-literal-string', rule, usual);
+ruleTester.run('no-literal-string', rule, jsx);
 
 //
 // ─── VUE ────────────────────────────────────────────────────────────────────────
@@ -231,15 +238,29 @@ const tsTester = new RuleTester({
   },
 });
 
+tsTester.run('no-literal-string', rule, usual);
+tsTester.run(
+  'no-literal-string',
+  rule,
+  Object.entries(jsx).reduce(
+    (prev, [key, value]) => ({
+      ...prev,
+      [key]: value.map(item => ({
+        ...item,
+        filename: 'a.jsx',
+      })),
+    }),
+    {}
+  )
+);
+
 tsTester.run('no-literal-string', rule, {
   valid: [
     { code: `declare module 'country-emoji' {}` },
     { code: '<div className="hello"></div>', filename: 'a.tsx' },
     { code: "var a: Element['nodeName']" },
     { code: "var a: Omit<T, 'af'>" },
-    { code: `var a: 'abc' = 'abc'` },
-    { code: `var a: 'abc' | 'name'  | undefined= 'abc'` },
-    { code: "type T = {name: 'b'} ; var a: T =  {name: 'b'}" },
+    { code: "function Button({ t= 'name'  }: {t: string}){} " },
     { code: "enum T  {howard=1, 'a b'=2} ; var a = T['howard']" },
     { code: "function Button({ t= 'name'  }: {t: 'name'}){} " },
     { code: "type T ={t?:'name'|'abc'};function Button({t='name'}:T){}" },
