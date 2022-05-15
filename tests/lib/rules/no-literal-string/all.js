dc33346a52c2ebdfc51fc00234ef2ee4d7be2766
Edvard Chen <pigeon73101@gmail.com>
2022-05-16 01:04:07 +0800

    fix: validate correctly call chains
    closes #54

--- tests/lib/rules/no-literal-string/all.js
+++ tests/lib/rules/no-literal-string/all.js
@@ -3,7 +3,19 @@ const runTest = require('../../helpers/runTest');
 
 const cases = {
   valid: [testFile('valid.jsx')],
-  invalid: [{ ...testFile('invalid.jsx'), errors: 13 }],
+  invalid: [
+    { ...testFile('invalid.jsx'), errors: 13 },
+    {
+      code: `export const validationSchema = Yup.object({
+        email: Yup
+          .string()
+          .email('hello')
+          .required('world'),
+      })`,
+      options: [{ mode: 'all' }],
+      errors: 2,
+    },
+  ],
 };
 
 runTest('no-literal-string: mode all', cases);
