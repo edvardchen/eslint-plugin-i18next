178b703100e6c13ceafb4fd1032696cae47e55c8
Edvard Chen <pigeon73101@gmail.com>
2022-05-18 14:21:01 +0800

    fix: support to ignore member expression in assignments

--- tests/lib/rules/no-literal-string/object-properties.js
+++ tests/lib/rules/no-literal-string/object-properties.js
@@ -1,8 +1,19 @@
-const testFile = require('../../helpers/testFile');
 const runTest = require('../../helpers/runTest');
 
 const cases = {
   valid: [
+    {
+      code: 'HomePage.displayName = "HomePage";',
+      options: [
+        {
+          mode: 'all',
+          'object-properties': {
+            include: [],
+            exclude: ['displayName'],
+          },
+        },
+      ],
+    },
     {
       code: 'var a = {foo: "foo"};',
       options: [{ mode: 'all', 'object-properties': { exclude: ['foo'] } }],
