2aceca0aa8067fb77b5bb1e057cbfef378e634ed
Mike <yum650350@gmail.com>
2026-02-23 15:50:07 +0800

    feat: support eslint v10

--- tests/lib/helpers/runTest.js
+++ tests/lib/helpers/runTest.js
@@ -1,16 +1,13 @@
 const RuleTester = require('eslint').RuleTester;
 const rule = require('../../../lib/rules/no-literal-string');
-const babelParser = require('@babel/eslint-parser');
 
 var ruleTester = new RuleTester({
   languageOptions: {
-    parser: babelParser,
     sourceType: 'module',
     ecmaVersion: 2022,
     parserOptions: {
-      requireConfigFile: false,
-      babelOptions: {
-        plugins: ['@babel/plugin-syntax-jsx'],
+      ecmaFeatures: {
+        jsx: true,
       },
     },
   },
